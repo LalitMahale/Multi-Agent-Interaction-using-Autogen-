2f8eaa8019bc131e61424bc85b7c202c80f6fda0
LalitMahale <lalitmahale121@gmail.com>
2024-10-29 14:30:54 +0530

    agent added

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -1,4 +1,17 @@
-import logging
+from autogen import ConversableAgent
+import autogen
+from ..configuration import config
+config_list = autogen.config_list_from_json(config.CONFIG_LIST)
 
-logging.info("Agent is Runing properly")
 
+agent = ConversableAgent(
+    name = "News collector",
+    llm_config= {"config_list":config_list},
+    code_execution_config=False,
+    human_input_mode="NEVER",
+    function_map= None
+)
+
+reply = agent.generate_reply({"content":"Who are you ?","role":"user"})
+
+print(reply)
